5be32444ee214ef25843539e05503e7ea39e5781
0427roger <0427roger@gmail.com>
2026-02-23 21:18:59 +0800

    1

--- docs/js/home.js
+++ docs/js/home.js
@@ -1,4 +1,6 @@
-async function init() {
+// home.js
+
+function init() {
   const timeline = document.getElementById("timeline");
 
   const updates = [
@@ -17,39 +19,8 @@ async function init() {
       <h3>${item.title}</h3>
       <small>${item.date}</small>
     `;
-  timeline.appendChild(div);
-});
-
-init();
-
-  const allItems = [];
-
-  data.sections.forEach(section => {
-    section.items.forEach(item => {
-      allItems.push({
-        tag: section.tag,
-        title: item.title,
-        path: item.path,
-        date: item.date
-      });
-    });
-  });
-
-  allItems.sort((a,b) => new Date(b.date) - new Date(a.date));
-
-  const timeline = document.getElementById("timeline");
-
-  allItems.forEach(item => {
-    const div = document.createElement("div");
-    div.className = "timeline-item";
-    div.innerHTML = `
-      <div class="tag">${item.tag}</div>
-      <h3>${item.title}</h3>
-      <small>${item.date}</small>
-    `;
-    div.onclick = () => {
-      window.location.href = `archive.html?path=${encodeURIComponent(item.path)}`;
-    };
     timeline.appendChild(div);
   });
 }
+
+init();
